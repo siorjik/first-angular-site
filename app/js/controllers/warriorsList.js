e501d0d00c66bd2146e2d6ef2e80811318c0174d
Siorjik <serjkaplun@gmail.com>
2017-07-20 20:17:04 +0300

    fixed damage and price 7 lvl

--- app/js/controllers/warriorsList.js
+++ app/js/controllers/warriorsList.js
@@ -24,17 +24,58 @@ app.controller("townUnitsCtrl", function(townsList, units, $scope, $uibModal, $w
 		for(var key in units) {
 			var unitStart = units[key].warUnitStart;
 			for(var k in unitStart) {
+				if(unitStart[k].damage) {
+					var damageStart = unitStart[k].damage.indexOf("-");
+					if(damageStart != -1) {
+						var partStart_1 = unitStart[k].damage.substring(0, damageStart);
+						var partStart_2 = unitStart[k].damage.substring(damageStart + 1);
+
+						if(partStart_1 && partStart_2) unitStart[k].sumDamage = (+partStart_1 + +partStart_2) / 2;
+					}
+					else unitStart[k].sumDamage = +unitStart[k].damage;
+				}
+				if(unitStart[k].price) {
+					var priceStart = unitStart[k].price.toString().indexOf("and");
+					if(priceStart != -1) {
+						var goldStart = unitStart[k].price.substring(0, priceStart - 1);
+						var resursStart = unitStart[k].price.substr(priceStart + 4, 1);
+
+						unitStart[k].priceResult = +goldStart + +resursStart;
+					}
+					else unitStart[k].priceResult = unitStart[k].price;
+				}
 				unitStart[k].town = units[key].town; 
 				startLvl.push(unitStart[k]);
 			}
+			
 			var unitUp = units[key].warUnitUp;
 			for(var i in unitUp) {
+				if(unitUp[i].damage) {
+					var damageUp = unitUp[i].damage.indexOf("-");
+					if(damageUp != -1) {
+						var partUp_1 = unitUp[i].damage.substring(0, damageUp);
+						var partUp_2 = unitUp[i].damage.substring(damageUp + 1);
+
+						if(partUp_1 && partUp_2) unitUp[i].sumDamage = (+partUp_1 + +partUp_2) / 2;
+					}
+					else unitUp[i].sumDamage = +unitUp[i].damage;
+				}
+				if(unitUp[i].price) {
+					var priceUp = unitUp[i].price.toString().indexOf("and");
+					if(priceUp != -1) {
+						var goldUp = unitUp[i].price.substring(0, priceUp - 1);
+						var resursUp = unitUp[i].price.substr(priceUp + 4, 1);
+
+						unitUp[i].priceResult = +goldUp + +resursUp;
+					}
+					else unitUp[i].priceResult = unitUp[i].price;
+				}
 				unitUp[i].town = units[key].town;
 				upLvl.push(unitUp[i])
 			}
 		}
+		
 		resArr = startLvl.concat(upLvl);
-		//console.log(resArr);
 		return resArr;
 	}
 	//выбор по-умолчанию в сравнении
